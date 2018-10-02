db2288fa3d359a1a110c3e91c0b162841dfca905
Mert Acel <mertacel@gmail.com>
2018-10-02 16:33:13 +0300

    CMAKE gecisleri yapildi

--- 01_Yaratilis_Creational/01_Singleton/01_Giris/build/CMakeFiles/Singleton-01_Giris.dir/cmake_clean.cmake
+++ 01_Yaratilis_Creational/01_Singleton/01_Giris/build/CMakeFiles/Singleton-01_Giris.dir/cmake_clean.cmake
@@ -1,8 +1,8 @@
 file(REMOVE_RECURSE
   "CMakeFiles/Singleton-01_Giris.dir/src/main.cpp.o"
   "CMakeFiles/Singleton-01_Giris.dir/src/Mesaj.cpp.o"
-  "Singleton-01_Giris.pdb"
-  "Singleton-01_Giris"
+  "../bin/Singleton-01_Giris.pdb"
+  "../bin/Singleton-01_Giris"
 )
 
 # Per-language clean rules from dependency scanning.
