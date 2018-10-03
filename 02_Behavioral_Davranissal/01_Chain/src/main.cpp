e372f126558a389ef7defae48f47a8ac67b08bb7
Mert Acel <mertacel@gmail.com>
2018-10-03 16:22:04 +0300

    README duzenlendi, Proje Isimleri Duzenlendi, Behavioral Pattern Sablon Olsturuldu

---
+++ 02_Behavioral_Davranissal/01_Chain/src/main.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// İsim        : 01_Chain
+// Yazan       : Mert AceL
+// Version     : 1.0
+// Copyright   : AceL
+// Açıklama    : Chain Pattern
+//============================================================================
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+  return 0;
+}
