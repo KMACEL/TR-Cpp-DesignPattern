822f8e89519e0813fdb0229c500d158c919ef453
Mert Acel <mert.acel@ardictech.com>
2018-10-16 14:28:27 +0300

    Strategy Pattern Sablonu Olusturuldu

--- 02_Behavioral_Davranissal/09_Strategy/src/main.cpp
+++ 02_Behavioral_Davranissal/09_Strategy/src/main.cpp
@@ -8,6 +8,79 @@
 #include <iostream>
 using namespace std;
 
+class OgretmenInterface
+{
+  public:
+    virtual void atama(string) const = 0;
+};
+
+class OgretmenA : public OgretmenInterface
+{
+  public:
+    void atama(string okul) const override
+    {
+        cout << "Öğretmen A, " << okul << " okuluna atandı..." << endl;
+    }
+};
+
+class OgretmenB : public OgretmenInterface
+{
+  public:
+    void atama(string okul) const override
+    {
+        cout << "Öğretmen B, " << okul << " okuluna atandı..." << endl;
+    }
+};
+
+class OgretmenC : public OgretmenInterface
+{
+  public:
+    void atama(string okul) const override
+    {
+        cout << "Öğretmen C, " << okul << " okuluna atandı..." << endl;
+    }
+};
+
+class Okul
+{
+  private:
+    OgretmenInterface *ogretmenInterface;
+    string okulIsmi;
+
+  public:
+    Okul(OgretmenInterface *ogretmenInterface, string okulIsmi) : ogretmenInterface(ogretmenInterface), okulIsmi(okulIsmi)
+    {
+    }
+
+    void setOgretmenDegistir(OgretmenInterface *ogretmenInterface)
+    {
+        this->ogretmenInterface = ogretmenInterface;
+    }
+
+    void atama() const
+    {
+        ogretmenInterface->atama(okulIsmi);
+    }
+};
+
 int main()
 {
+    OgretmenA ogretmenA;
+    OgretmenB ogretmenB;
+    OgretmenC ogretmenC;
+
+    Okul okul1(&ogretmenA, "Okul-1");
+    Okul okul2(&ogretmenB, "Okul-2");
+    Okul okul3(&ogretmenC, "Okul-3");
+
+    okul1.atama();
+    okul2.atama();
+    okul3.atama();
+
+    okul1.setOgretmenDegistir(&ogretmenB);
+    okul1.atama();
+    okul1.setOgretmenDegistir(&ogretmenC);
+    okul1.atama();
+
+    return 0;
 }
