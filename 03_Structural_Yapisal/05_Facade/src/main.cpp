abbd292551057f59a414a222a1b874cad723e615
Mert Acel <mertacel@gmail.com>
2018-10-16 16:57:30 +0300

    Facade Sablon Olusturuldu

--- 03_Structural_Yapisal/05_Facade/src/main.cpp
+++ 03_Structural_Yapisal/05_Facade/src/main.cpp
@@ -5,9 +5,86 @@
 // Copyright   : AceL
 // Açıklama    : Facade Pattern
 //============================================================================
+#include <string>
 #include <iostream>
+
 using namespace std;
+
+class Alarm
+{
+  public:
+    void alarmAc()
+    {
+        cout << "Alarm Açıldı. Eviniz güvende..." << endl;
+    }
+
+    void alarmKapat()
+    {
+        cout << "Alarm Kapatıldı..." << endl;
+    }
+};
+
+class Elektrik
+{
+  public:
+    void elektrikAc()
+    {
+        cout << "Elektrik Açık..." << endl;
+    }
+
+    void elektrikKapat()
+    {
+        cout << "Elektrik Kapalı..." << endl;
+    }
+};
+
+class Tv
+{
+  public:
+    void tvAc()
+    {
+        cout << "Tv Açık..." << endl;
+    }
+
+    void tvKapat()
+    {
+        cout << "TV Kapalı..." << endl;
+    }
+};
+
+class EvFacade
+{
+    Alarm alarm;
+    Elektrik elektrik;
+    Tv tv;
+
+  public:
+    EvFacade() {}
+
+    void evdenDisariCikinca()
+    {
+        elektrik.elektrikKapat();
+        tv.tvKapat();
+        alarm.alarmAc();
+    }
+
+    void eveDonunce()
+    {
+        alarm.alarmKapat();
+        elektrik.elektrikAc();
+        tv.tvAc();
+    }
+};
 
 int main()
 {
+    EvFacade evFacade;
+
+    cout << "Evden İşe Gidiliyor ..." << endl;
+    evFacade.evdenDisariCikinca();
+
+    cout << "---------------------------------------------" << endl;
+
+    cout << "Eve Giriliyor..." << endl;
+    evFacade.eveDonunce();
 }
