992e0b716254ea70a3343e1e11139c048dbba814
Mert Acel <mertacel@gmail.com>
2018-10-22 14:05:08 +0300

    Template Pattern Sablon Olusturuldu

--- 02_Behavioral_Davranissal/10_Template/src/main.cpp
+++ 02_Behavioral_Davranissal/10_Template/src/main.cpp
@@ -6,8 +6,76 @@
 // Açıklama    : Template Pattern
 //============================================================================
 #include <iostream>
+
 using namespace std;
+
+class AbstractClass
+{
+  public:
+    void templateMethod()
+    {
+        islem1();
+        islem2();
+        sabitIslem();
+        tercihliIslem();
+    }
+
+    virtual void islem1() = 0;
+    virtual void islem2() = 0;
+    virtual void tercihliIslem() {}
+    void sabitIslem()
+    {
+        cout << "Bu işlem sabit. Bütün sınıflarda aynı..." << endl;
+    }
+};
+
+class Sinif1 : public AbstractClass
+{
+  public:
+    void islem1()
+    {
+        cout << "Sinif1 - islem1 çalıştı..." << endl;
+    }
+    void islem2()
+    {
+        cout << "Sinif1 - islem2 çalıştı..." << endl;
+    }
+    void tercihliIslem()
+    {
+        cout << "Sinif1 - tercihliIslem çalıştı..." << endl;
+    }
+    void sabitIslem()
+    {
+        cout << "Bu baya sabit" << endl;
+    }
+};
+
+class Sinif2 : public AbstractClass
+{
+  public:
+    void islem1()
+    {
+        cout << "Sinif2 - islem1 çalıştı..." << endl;
+    }
+    void islem2()
+    {
+        cout << "Sinif2 - islem2 çalıştı..." << endl;
+    }
+    void sabitIslem()
+    {
+        cout << "Bu baya sabit" << endl;
+    }
+};
 
 int main()
 {
+    Sinif1 sinif1;
+    Sinif2 sinif2;
+
+    sinif1.templateMethod();
+
+    cout << "-------------------------------" << endl;
+    sinif2.templateMethod();
+
+    return 0;
 }
