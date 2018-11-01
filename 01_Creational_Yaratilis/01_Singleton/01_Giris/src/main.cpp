6ad845d3a5d63ff45cae65893445f128f71f5592
Mert Acel <mertacel@gmail.com>
2018-11-01 16:39:40 +0300

    Singleton Aciklamaya Baslandi

--- 01_Creational_Yaratilis/01_Singleton/01_Giris/src/main.cpp
+++ 01_Creational_Yaratilis/01_Singleton/01_Giris/src/main.cpp
@@ -7,9 +7,54 @@
 //============================================================================
 #include <iostream>
 #include <string>
-#include "Mesaj.h" // Hazırladığımız sınıfı ekliyoruz
 
 using namespace std;
+
+class Mesaj
+{
+private:
+  // INSTANCE değişkenine başlangıç değeri atıyoruz
+  static Mesaj *INSTANCE;
+  string mesaj;
+
+protected:
+  // Constructor Methodumuz
+  Mesaj()
+  {
+  }
+
+public:
+  // Mesaj sınıfımızı kullanmak için oluşturduğumuz public türündeki method
+  static Mesaj *getInstance()
+  {
+    // Eğer INSTANCE değeri 0 ise yani hiç bu sınıf çağırılmamışsa,
+    // sınıfı oluşturuyoruz.
+    if (INSTANCE == 0)
+    {
+      INSTANCE = new Mesaj();
+    }
+    // Eğer bu sınıf daha öncd oluşturulmuşsa, yani çağırılmışsa geriye
+    // var olan "INSTANCE"yi döndürüyoruz
+    return INSTANCE;
+  }
+  // mesaj değişkenine veri atamamıza yardımcı olan method
+  void setMesaj(string mesaj)
+  {
+    this->mesaj = mesaj;
+  }
+  // mesaj değişkeninden veri almaya yarayan method
+  string getMesaj()
+  {
+    return this->mesaj;
+  }
+  // mesaj değişken içeriğini yazmaya yarayan method
+  void mesajYazir()
+  {
+    cout << "Mesajınız : " << getMesaj() << endl;
+  }
+};
+
+Mesaj *Mesaj::INSTANCE=0;
 
 int main()
 {
@@ -23,7 +68,7 @@ int main()
   Mesaj::getInstance()->setMesaj("Merhaba");
   Mesaj::getInstance()->mesajYazir();
 
-  // Burada da gösterilmek istenen, tekrar bir nesne yaratılmasına rağmen ve 
+  // Burada da gösterilmek istenen, tekrar bir nesne yaratılmasına rağmen ve
   // setMesaj() fonksiyonunu kullanmamamıza rağmen, bir önceki hali oluyor.
   Mesaj *m2 = Mesaj::getInstance();
   m2->mesajYazir();
---
+++ 01_Creational_Yaratilis/01_Singleton/README.md
@@ -0,0 +1,7 @@
+#Singleton Pattern
+
+**Singleton** kelime anlamı olarak dilimize **"Tek Şey"** şeklinde çevrilebilir. İsimdende yola çıkarak, aslında **Tek** sınıf olmasıdır. Yani nereden çağırılırsa çağırılsın tek sonuç, aynı sonuç dönme durumudur. Kısaca bir **Singleton** sınıfını nerede çağırırsanız çağırın, hepsinde aynı şekilde işlemler sağlayacaktır. Bu patterni bir çok projede görebilirsiniz. 
+
+Singleton patternini 3 porje olarak göreceksiniz. Bunun sebebi, bu pattern multithread yapılarda bazı istenmeyen sonuçlar sağlama olasılığıdır.
+
+## Singleton Pattern Kullanım Alanı
